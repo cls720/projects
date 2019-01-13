e856f4704263dbe68e85a1d5319de30493e2088e
cls <cls720@126.com>
2019-01-13 11:38:57 +0800

    add data

---
+++ gp-table/examples/mock/xm-zjfp-data.js
@@ -0,0 +1,17 @@
+// 项目资金分配表数据
+module.exports = [{
+    "year": 2018,
+    "fjd": 1,
+    "month": 1,
+    "week": 1,
+    "fc8": "电气",
+    "fc10": "周",
+    "FYNUM": "YEAR-TASK-001",
+    "FQNUM": "JCRW20180101103920",
+    "FMNUM": "JCRW20180101103954",
+    "FWNUM": "JCRW20180101104057",
+    "FC1": null,
+    "FC2": "未完成",
+    "FC3": "未完成",
+    "FC4": "未完成"
+}]
