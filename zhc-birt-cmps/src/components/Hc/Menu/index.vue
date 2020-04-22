afb1a9cea25f33754a80af07c250fcd26006f407
cls720 <cls720@126.com>
2020-04-22 23:36:51 +0800

    asd

--- zhc-birt-cmps/src/components/Hc/Menu/index.vue
+++ zhc-birt-cmps/src/components/Hc/Menu/index.vue
@@ -27,12 +27,13 @@ import HcCmpt from "@/components/Hc/Cmpt";
 import events from "@/components/mixins/events";
 import autosize from "@/components/mixins/autosize";
 import datasource from "@/components/mixins/datasource";
+import treedata from "@/components/mixins/treedata";
 
-import { randomString } from '@/utils/StringUtil.js'
+import { randomString } from "@/utils/StringUtil.js";
 
 export default {
   name: "hc-menu",
-  mixins: [events, autosize, datasource],
+  mixins: [events, autosize, datasource, treedata],
   extends: HcCmpt,
   computed: {
     confStyle() {
@@ -61,15 +62,29 @@ export default {
         console.error("找不到elmenu实例");
       }
     },
-    menuChildren() {      
+    menuChildren() {
       let menus = this.conf.children || [];
+      let treeData = this.getConvertTreeData();
+      let me = this;
       this.datas.forEach(recd => {
         let menuItem = {};
         if (recd.children && recd.children.length > 0) {
           menuItem.controlName = "HcSubMenu";
           menuItem.controlId = "HcSubMenu" + randomString(6);
+          menuItem.title = recd[me.labelField];
+          Object.assign(menuItem, recd);
         }
       });
+    },
+    addSubMenu(menu, recd) {
+      let menuItem = {};
+      if (recd.children && recd.children.length > 0) {
+        menuItem.controlName = "HcSubMenu";
+        menuItem.controlId = "HcSubMenu" + randomString(6);
+        menuItem.title = recd[this.labelField];
+        Object.assign(menuItem, recd);
+        this.addSubMenu();
+      }
     }
   }
 };
